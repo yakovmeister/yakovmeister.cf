1c1fb20c69e4ae43be0c40ffb5b2ddae039f4066
yakovmeister <electro7bug@gmail.com>
2016-11-26 21:59:54 +0800

    filling it more

--- resources/views/temp/index.blade.php
+++ resources/views/temp/index.blade.php
@@ -9,13 +9,29 @@
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
-	<div class="row">
-		<div class="page-header" align="center">	
-			<h1 class="fancy-font-1">Yakovmeister</h1>
-			<p class="fancy-font-3">Cuz smplct iz owezum!</p>
-			<p class="fancy-font-3">Also Yes, Fancy I cannot.</p>
-			<a href="https://www.facebook.com/yakovmeister" style="color:#3b5998;"><span class="fa fa-facebook-square"></span></a> | 
-			<a href="https://github.com/yakovmeister" style="color:#000000;"><span class="fa fa-github"></span></a>
+	<div class="container">
+		<div class="row">
+			<div class="page-header" align="center">	
+				<h1 class="fancy-font-1">Yakovmeister</h1>
+				<p class="fancy-font-3">Cuz smplct iz owezum!</p>
+				<p class="fancy-font-3">Also Yes, Fancy I cannot.</p>
+				<a href="https://www.facebook.com/yakovmeister" style="color:#3b5998;">
+					<span class="fa fa-facebook-square"></span>
+				</a> | 
+				<a href="https://github.com/yakovmeister" style="color:#000000;">
+					<span class="fa fa-github"></span>
+				</a> | 
+				<a href="https://paypal.me/yakovmeister/500" style="color:#1e477a;">
+					<span class="fa fa-paypal"></span>
+				</a>
+			</div>
+		</div>
+		<div class="row">
+			<h3 class="fancy-font-1" align="center" style="width: 60%; margin: 0 auto;">This is just a temporary homepage while the other one is still under development, though the next pages you'll see in the future may be as simple as this or maybe simpler. . . Or maybe this is the last page you'll see. Who knows and who cares?</h3>
+			<br />
+			<h4 class="fancy-font-1" align="center" style="width:40%; margin: 0 auto;">I'm broke. . . I'd gladly accept your donations <a href="https://paypal.me/yakovmeister/">here</a>.</h4>
+			<br />
+			<h5 class="fancy-font-1" align="center" style="width:40%; margin: 0 auto;">Lastly, check the mediocrity of my works <a href="https://github.com/yakovmeister">here</a>.</h5>
 		</div>
 	</div>
 	<!-- Javascript -->
