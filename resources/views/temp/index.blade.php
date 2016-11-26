f6e5dfd4e44da5728b15bebef01c3dcdbfd0d6c1
yakovmeister <electro7bug@gmail.com>
2016-11-26 21:38:20 +0800

    fill temp.index

--- resources/views/temp/index.blade.php
+++ resources/views/temp/index.blade.php
@@ -1,9 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Oh! Not this Page</title>
+	<meta name="description" content="text">
+
+	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+	<link  href="{{ asset('/css/theme.css') }}" rel="stylesheet">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
-
+	<div class="row">
+		<div class="page-header" align="center">	
+			<h1 class="fancy-font-1">Yakovmeister</h1>
+			<p class="fancy-font-3">Cuz smplct iz owezum!</p>
+			<p class="fancy-font-3">Also Yes, Fancy I cannot.</p>
+			<a href="https://www.facebook.com/yakovmeister" style="color:#3b5998;"><span class="fa fa-facebook-square"></span></a> | 
+			<a href="https://github.com/yakovmeister" style="color:#000000;"><span class="fa fa-github"></span></a>
+		</div>
+	</div>
+	<!-- Javascript -->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-1.x-git.min.js"></script>
+	<script src="{{ asset('/js/app.js') }}" rel="stylesheet"></script>
 </body>
 </html>
