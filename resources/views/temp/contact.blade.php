5a01a011fa1044fb594be8eefc81de78fb459c99
yakovmeister <electro7bug@gmail.com>
2016-11-29 04:36:31 +0800

    contact-me: updated links

--- resources/views/temp/contact.blade.php
+++ resources/views/temp/contact.blade.php
@@ -2,17 +2,19 @@
 
 @section('content')
 		<div class="row">
-			<div class="col-md-6">
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
 				<h3 class="fancy-font-1">Facebook: <a href="https://www.facebook.com/yakovmeister">yakovmeister</a></h3>
 				<h3 class="fancy-font-1">Twatter: <a href="https://www.twitter.com/yakovmeister">@yakovmeister</a></h3>
-				<h3 class="fancy-font-1">Spotify: <a href="#">yakovmeister</a></h3>
-				<h3 class="fancy-font-1">Steam: <a href="#">yakovmeister</a></h3>
+				<h3 class="fancy-font-1">Spotify: <a href="http://open.spotify.com/user/yakovmeister">yakovmeister</a></h3>
+				<h3 class="fancy-font-1">Steam: <a href="https://steamcommunity.com/id/yakovmeister/">yakovmeister</a></h3>
 				<h3 class="fancy-font-1">PayPal: <a href="https://paypal.me/yakovmeister/1000">yakovmeister</a></h3>
 				<h3 class="fancy-font-1">GitHub: <a href="https://github.com/yakovmeister">yakovmeister</a></h3>
 			</div>
-			<div class="col-md-6">
+			<div class="col-md-4">
 				<h3 class="fancy-font-1">Mobile Number: +639330432690</h3>
-				<h3 class="fancy-font-1" title="Deal with it">Email: partypornstar@mail2tor.com</h3>
+				<h3 class="fancy-font-1" title="Deal with it or alternatively email me at: electro7bug@gmail.com">Email: partypornstar@mail2tor.com</h3>
 			</div>
+			<div class="col-md-2"></div>
 		</div>
 @stop
